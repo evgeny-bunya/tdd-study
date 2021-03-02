d7a80cdbc4ca0d609c5dc56562dad8eae2d05cda
Evgeniy Bunya <bunya.evgeniy@gmail.com>
2021-03-02 23:46:22 +0300

    currency class
    add possibility to compare various currencies

--- Currency.hpp
+++ Currency.hpp
@@ -1,33 +1,58 @@
-#ifndef CURRENCY_HPP
+ #ifndef CURRENCY_HPP
 #define CURRENCY_HPP
 
-class Dollar
+enum class Currency
+{
+    Dollar,
+    Franc
+};
+
+class Money
 {
 public:
-    explicit Dollar(int initialValue):
-        amount(initialValue)
+    bool isEqual(const Money& compare) const
     {
+        return (amount == compare.amount) && (currency == compare.currency);
+    }
+protected:
+    int amount;
+    Currency currency;
+};
 
+class Dollar : public Money
+{
+public:
+    explicit Dollar(int initialValue)
+    {
+        amount = initialValue;
+        currency = Currency::Dollar;
     }
     Dollar times(int multiplicator)
     {
         return Dollar(amount * multiplicator);
     }
-    bool isEqual(const Dollar& compare) const
-    {
-        return (amount == compare.amount);
-    }
-private:
-    int amount;
 };
 
-bool operator==(const Dollar& lhs, const Dollar& rhs)
+class Franc : public Money
+{
+public:
+    explicit Franc(int initialValue)
+    {
+        amount = initialValue;
+        currency = Currency::Franc;
+    }
+    Franc times(int multiplicator)
+    {
+        return Franc(amount * multiplicator);
+    }
+};
+
+bool operator==(const Money& lhs, const Money& rhs)
 {
     return lhs.isEqual(rhs);
 }
-bool operator!=(const Dollar& lhs, const Dollar& rhs)
+bool operator!=(const Money& lhs, const Money& rhs)
 {
-    return !lhs.isEqual(rhs);
+    return !(lhs ==rhs);
 }
-
 #endif // CURRENCY_HPP
--- currency_test.cpp
+++ currency_test.cpp
@@ -15,7 +15,17 @@ TEST(Currency, testMultiplication)
 TEST(Currency, equalityCheck)
 {
     EXPECT_EQ(*std::make_unique<Dollar>(7), *std::make_unique<Dollar>(7));
-    //EXPECT_EQ(*std::make_unique<Dollar>(7), *std::make_unique<Dollar>(7));
     EXPECT_NE(*std::make_unique<Dollar>(7), *std::make_unique<Dollar>(8));
+    EXPECT_EQ(*std::make_unique<Franc>(7), *std::make_unique<Franc>(7));
+    EXPECT_NE(*std::make_unique<Franc>(7), *std::make_unique<Franc>(8));
+    EXPECT_NE(*std::make_unique<Franc>(7), *std::make_unique<Dollar>(7));
 }
+
+TEST(Currency, francTestMultiplication)
+{
+    auto fiveFrancs = std::make_unique<Franc>(5);
+    EXPECT_EQ(fiveFrancs->times(2), *std::make_unique<Franc>(10));
+    EXPECT_EQ(fiveFrancs->times(3), *std::make_unique<Franc>(15));
+}
+
 
